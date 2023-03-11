666522f5ff38718875b2d1ad5e9728d0de6f6f19
ctruo <ctruongg67@gmail.com>
2023-03-10 22:50:06 -0800

    Added arrow to indicate dropdown menu

--- frontend/public/client-scripts/main-page.js
+++ frontend/public/client-scripts/main-page.js
@@ -2,13 +2,21 @@
 
 const inputBox = document.querySelector(".pet-search");
 const list = document.getElementById("pet-options");
+const arrow = document.getElementsByClassName("fa-angle-up");
 
 function showList() {
   list.classList.toggle("hide"); //toggles hide class on and off when input box is clicked
+  arrow[0].classList.toggle("fa-active"); //flips arrow to down when showing menu
 }
 
 function setValue(value) {
+  //sets input box to clicked on value for query
   inputBox.value = value;
-
-  list.classList.toggle("hide"); //hides menu after clicking on option
 }
+
+document.addEventListener("click", (event) => {
+  if (event.target != inputBox) {
+    list.classList.add("hide"); //hides menu when clicked off of input box
+    arrow[0].classList.remove("fa-active"); //flips arrow back up when menu is hidden
+  }
+});
